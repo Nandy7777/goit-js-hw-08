09bc44d71bdb69c3ce3bb13927b16bd6ddf5540c
Nandy7777 <nandy7777@ukr.net>
2022-08-20 16:37:19 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
+const galleryImages = createGallleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryImages);
+
+function createGallleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+                </a>
+                </div>`;
+    })
+    .join(``);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
