7b266b69ed77dee5ecf9433baddd1ba37de1b231
Nandy7777 <nandy7777@ukr.net>
2022-08-20 19:46:56 +0300

    some 2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+// import throttle from `lodash.throttle`;
 
+const iframe = document.querySelector(`iframe`);
+const iframePlayer = new Player(iframe);
+
+iframePlayer.on('timeupdate', function () {
+  console.log(`Працює!`);
+  // data is an object containing properties specific to that event
+}); 
+
+
+localStorage.setItem('videoplayer-current-time', JSON.stringify('timeupdate'));
+
+const saveData = localStorage.getItem('videoplayer-current-time');
+console.log(`saveData`, saveData);
+
+const parseData = JSON.parse(saveData);
+console.log(`parseData`, parseData);
+
+iframePlayer
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from "lodash.throttle";
 
+
+const form = document.querySelector(`.feedback-form`);
+const input = document.querySelector(`.feedback-form input`);
+const textarea = document.querySelector(`.feedback-form textarea`);
+const button = document.querySelector(`.feedback-form button`);
+
+const formData = {};
+
+const STORAGE_KEY = `feedback-form-state`;
+
+form.addEventListener(`submit`, onFormSubmit);
+input.addEventListener(`input`, onInputInput);
+textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+
+form.addEventListener(`input`, e => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+});
+
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(`надсилаєм форму`);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onInputInput(evt) {
+
+};
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, message);
+
+};
+
+function populateTextarea() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+        console.log(saveMessage);
+        textarea.value = saveMessage;
+    }
+};
